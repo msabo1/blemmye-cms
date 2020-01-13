932b48f8bf35e404b085b96e51070f41c57e43f8
Mario Sabo <mariosabo2@gmail.com>
2020-01-13 23:01:37 +0100

    Generate roles module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ormConfig } from './config/typeorm.config';
+import { RolesModule } from './roles/roles.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(ormConfig),
+    RolesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
