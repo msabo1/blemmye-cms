42a96a401d8ecda0dcbfee9f25bc31cb96c555c7
Mario Sabo <mariosabo2@gmail.com>
2020-01-14 22:19:54 +0100

    Import entities in db connection

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,7 +1,8 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { config } from 'node-config-ts';
+import { entities } from '../ormentities'
 
 export const ormConfig: TypeOrmModuleOptions = {
   ...config.database.connection,
-  entities: []
+  entities
 }
