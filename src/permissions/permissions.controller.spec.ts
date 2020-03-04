2c404101748332a9f24ba704c3815fc95b6226c6
Mario Sabo <mariosabo2@gmail.com>
2020-03-04 19:54:12 +0100

    Write unit tests for permissions

--- src/permissions/permissions.controller.spec.ts
+++ src/permissions/permissions.controller.spec.ts
@@ -1,18 +1,46 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PermissionsController } from './permissions.controller';
+import { PermissionsService } from './permissions.service';
+import { async } from 'rxjs/internal/scheduler/async';
+import { InternalServerErrorException, BadGatewayException } from '@nestjs/common';
+
+const mockPermissionService = () => ({
+  findAll: jest.fn()
+});
 
 describe('Permissions Controller', () => {
   let controller: PermissionsController;
+  let permissionsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PermissionsController],
+      providers: [
+        {provide: PermissionsService, useFactory: mockPermissionService}
+      ]
     }).compile();
 
     controller = module.get<PermissionsController>(PermissionsController);
+    permissionsService = module.get<PermissionsService>(PermissionsService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('returns all permissions using permissionService', async () => {
+    let mockValue = "test value";
+    permissionsService.findAll.mockResolvedValue(mockValue);
+
+    expect(permissionsService.findAll).not.toBeCalled();
+    let result = await controller.getAll();
+    expect(permissionsService.findAll).toBeCalled();
+    expect(result).toEqual(mockValue);
+  });
+
+  it('returns error object due to permissionService failure', async () => {
+    permissionsService.findAll.mockImplementation(() => {throw new InternalServerErrorException});
+
+    expect(controller.getAll()).rejects.toThrow(InternalServerErrorException);
   });
 });
--- src/permissions/permissions.service.spec.ts
+++ src/permissions/permissions.service.spec.ts
@@ -1,18 +1,50 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PermissionsService } from './permissions.service';
+import { Permission } from './permission.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { InternalServerErrorException } from '@nestjs/common';
+
+const mockPermissionRepository = () => ({
+  find: jest.fn()
+});
 
 describe('PermissionsService', () => {
   let service: PermissionsService;
+  let permissionRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PermissionsService],
+      providers: [
+        PermissionsService,
+        { provide: getRepositoryToken(Permission) , useFactory: mockPermissionRepository }
+      ],
     }).compile();
 
     service = module.get<PermissionsService>(PermissionsService);
+    permissionRepository = module.get(getRepositoryToken(Permission));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  it('gets all permissions from repository', async () => {
+    let mockValue = 'test value';
+    permissionRepository.find.mockResolvedValue(mockValue);
+
+    expect(permissionRepository.find).not.toBeCalled();
+    let result = await service.findAll();
+    expect(permissionRepository.find).toBeCalled();
+    expect(result).toEqual(mockValue);
+  });
+
+  it('throws InternalServerErrorException', async () => {
+    permissionRepository.find.mockRejectedValue(null);
+
+    expect(service.findAll()).rejects.toThrow(InternalServerErrorException);
+    expect(permissionRepository.find).toBeCalled();
+
+  });
+
+
 });
