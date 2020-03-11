7d6e5d5e5b92f98afbc7e1a737bff61d0a2b6abf
Mario Sabo <mariosabo2@gmail.com>
2020-03-11 16:42:16 +0100

    Create get groups endpoint

--- src/groups/groups.controller.ts
+++ src/groups/groups.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { GroupsService } from './groups.service';
+import { Group } from './group.entity';
 
 @Controller('groups')
-export class GroupsController {}
+export class GroupsController {
+    constructor(private readonly groupsService: GroupsService){}
+
+    @Get()
+    async getAll(): Promise<Group[]>{
+        return await this.groupsService.findAll();
+    }
+}
--- src/groups/groups.module.ts
+++ src/groups/groups.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { GroupsService } from './groups.service';
 import { GroupsController } from './groups.controller';
+import { Group } from './group.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Group])],
   providers: [GroupsService],
   controllers: [GroupsController]
 })
