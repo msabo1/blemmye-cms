54b35d9e564db618935270205a6d447b0811a7de
Mario Sabo <mariosabo2@gmail.com>
2020-03-11 13:47:36 +0100

    Create groupsService and findAll logic

--- src/groups/groups.service.ts
+++ src/groups/groups.service.ts
@@ -1,4 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { Group } from './group.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class GroupsService {}
+export class GroupsService {
+
+    constructor(@InjectRepository(Group) private readonly groupRepository: Repository<Group>){}
+
+    async findAll(): Promise<Group[]>{
+        try{
+            const groups: Group[] = await this.groupRepository.find();
+            return groups;
+        }catch(error){
+            throw new InternalServerErrorException;
+        }
+    }
+}
