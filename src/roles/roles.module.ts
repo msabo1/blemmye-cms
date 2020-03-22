a50af9193d4744fdf31b5d6a596a173e0c3591d0
Mario Sabo <mariosabo2@gmail.com>
2020-03-22 20:06:12 +0100

    Import groups and permissions modules

--- src/roles/roles.module.ts
+++ src/roles/roles.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { RolesController } from './roles.controller';
 import { RolesService } from './roles.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { RolePrivilege } from './role-privilege';
+import { RoleRepository } from './role.repository';
+import { PermissionsModule } from '../permissions/permissions.module';
+import { GroupsModule } from '../groups/groups.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature()],
+  imports: [
+    TypeOrmModule.forFeature([RoleRepository, RolePrivilege]),
+    PermissionsModule,
+    GroupsModule
+  ],
   controllers: [RolesController],
   providers: [RolesService]
 })
