17502e215b4baf34fe76ecb64018f35e9dfef0c2
Mario Sabo <mariosabo2@gmail.com>
2020-03-22 20:02:40 +0100

    Create role repository and implement findWithQuery method

---
+++ src/roles/role.repository.ts
@@ -0,0 +1,33 @@
+import { EntityRepository, Repository } from "typeorm";
+import { Role } from "./role.entity";
+import { QueryRoleDto } from "./dto/query-role.dto";
+
+@EntityRepository(Role)
+export class RoleRepository extends Repository<Role>{
+    async findWithQuery(queryRoleDto: QueryRoleDto): Promise<Role[]>{
+        let {search, limit, offset, sortBy, order} = queryRoleDto;
+        const query = this.createQueryBuilder('role')
+            .leftJoinAndSelect('role.privileges', 'privilege')
+            .leftJoinAndSelect('privilege.permission', 'permission')
+            .leftJoinAndSelect('privilege.group', 'group');
+
+        if(search){
+            search = search.toLowerCase();
+            query.where('LOWER(role.name) LIKE :search OR LOWER(permission.name) LIKE :search OR LOWER(group.name) LIKE :search', {search: `%${search}%`});
+        }
+        if(offset){
+            query.skip(offset);
+        }
+        if(limit){
+            query.take(limit);
+        }
+        if(sortBy){
+            sortBy = 'role.' + sortBy;
+            query.orderBy(sortBy, order)
+        }
+
+        const roles = await query.getMany();
+
+        return roles;
+    }
+}
