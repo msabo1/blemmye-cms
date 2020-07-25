0f761ae59b4efbecdc5822e7ea08a0236baaec42
Mario Sabo <mariosabo2@gmail.com>
2020-07-25 19:46:25 +0200

    Create comments module

--- src/blog/blog.module.ts
+++ src/blog/blog.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PostsModule } from './posts/posts.module';
 import { CategoriesModule } from './categories/categories.module';
+import { CommentsModule } from './comments/comments.module';
 
 @Module({
-  imports: [PostsModule, CategoriesModule]
+  imports: [PostsModule, CategoriesModule, CommentsModule]
 })
 export class BlogModule {}
