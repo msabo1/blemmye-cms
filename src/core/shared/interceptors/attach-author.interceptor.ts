55bc8aed0756c1253257240c14182ec60f8120c2
Mario Sabo <mariosabo2@gmail.com>
2020-08-20 11:42:03 +0200

    Fix attach null author

--- src/core/shared/interceptors/attach-author.interceptor.ts
+++ src/core/shared/interceptors/attach-author.interceptor.ts
@@ -7,7 +7,7 @@ export class AttachAuthorInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     const request = context.switchToHttp().getRequest();
     const user: User = request.user;
-    request.body.authorId = user.id;
+    request.body.authorId = user?.id;
     return next.handle()
   }
 }
